bce64f6cea0a0b8e22bd5972d83cd1dc504ef442
SunTeng <teng_sun@qq.com>
2021-04-06 13:20:28 +0800

    添加gitignore

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy"
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:XDUSunteng/XDUSunteng.github.io.git master
