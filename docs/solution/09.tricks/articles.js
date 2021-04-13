727523ec7a5217a4807d3b5bafcc75b48daab978
SunTeng <teng_sun@qq.com>
2021-04-13 17:10:01 +0800

    添加数颜色

--- docs/solution/09.tricks/articles.js
+++ docs/solution/09.tricks/articles.js
@@ -2,6 +2,7 @@ module.exports = {
     getTricksSidebar: () => [{
         title: 'C++ STL',
         children: [
+			'/solution/09.tricks/9.1 C++ STL/题解-数颜色-Vector.md',
 			'/solution/09.tricks/9.1 C++ STL/题解-邻值查找-Set.md',
         ],
     }],
