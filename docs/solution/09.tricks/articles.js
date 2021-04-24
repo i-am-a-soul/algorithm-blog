455cec02b009abde680ed06f4ed66b0d9e70fdbd
SunTeng <teng_sun@qq.com>
2021-04-24 10:39:50 +0800

    添加普通平衡树-vector

--- docs/solution/09.tricks/articles.js
+++ docs/solution/09.tricks/articles.js
@@ -3,6 +3,7 @@ module.exports = {
         title: 'C++ STL',
         children: [
 			'/solution/09.tricks/9.1 C++ STL/题解-数颜色-Vector.md',
+			'/solution/09.tricks/9.1 C++ STL/题解-普通平衡树-Vector.md',
 			'/solution/09.tricks/9.1 C++ STL/题解-邻值查找-Set.md',
 			'/solution/09.tricks/9.1 C++ STL/题解-会场预约-Set.md',
 			'/solution/09.tricks/9.1 C++ STL/题解-砝码称重-Bitset.md',
