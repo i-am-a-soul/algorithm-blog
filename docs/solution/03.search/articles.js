ef221964ddb6f9e27234372f48b8d5fa999955f0
SunTeng <teng_sun@qq.com>
2021-04-25 11:17:52 +0800

    添加生日蛋糕

--- docs/solution/03.search/articles.js
+++ docs/solution/03.search/articles.js
@@ -2,21 +2,22 @@ module.exports = {
     getSearchSidebar: () => [{
         title: '树与图的遍历',
         children: [
-            '/solution/03.search/3.1 树与图的遍历/题解-可达性统计-反图 拓扑排序 bitset.md',
+            '/solution/03.search/3.1 树与图的遍历/题解-可达性统计-反图+拓扑排序+Bitset.md',
 			'/solution/03.search/3.1 树与图的遍历/题解-封锁阳光大学-图染色.md',
 			'/solution/03.search/3.1 树与图的遍历/题解-信息传递-拓扑排序+DFS.md',
         ],
     }, {
         title: '深度优先搜索',
         children: [
-            '/solution/03.search/3.2 深度优先搜索/题解-小猫爬山-dfs.md',
-			'/solution/03.search/3.2 深度优先搜索/题解-分成互质组-dfs.md',
-			'/solution/03.search/3.2 深度优先搜索/题解-数独-dfs.md',
+            '/solution/03.search/3.2 深度优先搜索/题解-小猫爬山-DFS.md',
+			'/solution/03.search/3.2 深度优先搜索/题解-分成互质组-DFS.md',
+			'/solution/03.search/3.2 深度优先搜索/题解-数独-DFS.md',
         ],
     }, {
         title: '剪枝',
         children: [
-			'/solution/03.search/3.3 剪枝/题解-木棒-dfs + 剪枝.md',
+			'/solution/03.search/3.3 剪枝/题解-木棒-DFS+剪枝.md',
+			'/solution/03.search/3.3 剪枝/题解-生日蛋糕-DFS+剪枝.md',
         ],
     }, {
         title: '迭代加深',
