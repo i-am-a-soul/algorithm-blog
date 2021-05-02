03c2bd675f7ede191c7c0c4654c63bf5f7b86ddd
SunTeng <teng_sun@qq.com>
2021-05-02 16:04:47 +0800

    添加状态机dp文件夹

--- docs/solution/06.dynamic-programming/articles.js
+++ docs/solution/06.dynamic-programming/articles.js
@@ -26,72 +26,76 @@ module.exports = {
 			'/solution/06.dynamic-programming/6.2 背包/题解-能量石-贪心+背包.md',
         ],
     }, {
+        title: '状态机DP',
+        children: [
+        ],
+    }, {
         title: '区间DP',
         children: [
-			'/solution/06.dynamic-programming/6.3 区间DP/题解-多边形-区间DP.md',
-			'/solution/06.dynamic-programming/6.3 区间DP/题解-金字塔-区间DP.md',
+			'/solution/06.dynamic-programming/6.4 区间DP/题解-多边形-区间DP.md',
+			'/solution/06.dynamic-programming/6.4 区间DP/题解-金字塔-区间DP.md',
         ],
     }, {
         title: '树形DP',
         children: [
-			'/solution/06.dynamic-programming/6.4 树形DP/题解-积蓄程度-换根法.md',
+			'/solution/06.dynamic-programming/6.5 树形DP/题解-积蓄程度-换根法.md',
         ],
     }, {
         title: '环形与后效性处理',
         children: [
-			'/solution/06.dynamic-programming/6.5 环形与后效性处理/题解-休息时间-环形处理.md',
-			'/solution/06.dynamic-programming/6.5 环形与后效性处理/题解-坏掉的机器人-后效性处理.md',
+			'/solution/06.dynamic-programming/6.6 环形与后效性处理/题解-休息时间-环形处理.md',
+			'/solution/06.dynamic-programming/6.6 环形与后效性处理/题解-坏掉的机器人-后效性处理.md',
         ],
     }, {
         title: '状态压缩DP',
         children: [
-			'/solution/06.dynamic-programming/6.6 状态压缩DP/题解-最短Hamilton路径-状压DP.md',
-            '/solution/06.dynamic-programming/6.6 状态压缩DP/题解-蒙德里安的梦想-状压DP.md',
-            '/solution/06.dynamic-programming/6.6 状态压缩DP/题解-炮兵阵地-状压DP_解法一_.md',
-            '/solution/06.dynamic-programming/6.6 状态压缩DP/题解-炮兵阵地-状压DP_解法二_.md',
+			'/solution/06.dynamic-programming/6.7 状态压缩DP/题解-最短Hamilton路径-状压DP.md',
+            '/solution/06.dynamic-programming/6.7 状态压缩DP/题解-蒙德里安的梦想-状压DP.md',
+            '/solution/06.dynamic-programming/6.7 状态压缩DP/题解-炮兵阵地-状压DP_解法一_.md',
+            '/solution/06.dynamic-programming/6.7 状态压缩DP/题解-炮兵阵地-状压DP_解法二_.md',
         ],
     }, {
         title: '倍增优化DP',
         children: [
-            '/solution/06.dynamic-programming/6.7 倍增优化DP/题解-计算重复-倍增优化.md',
+            '/solution/06.dynamic-programming/6.8 倍增优化DP/题解-计算重复-倍增优化.md',
         ],
     }, {
         title: '数据结构优化DP',
         children: [
-			'/solution/06.dynamic-programming/6.8 数据结构优化DP/题解-清理班次-线段树优化.md',
-			'/solution/06.dynamic-programming/6.8 数据结构优化DP/题解-清理班次2-线段树优化.md',
-			'/solution/06.dynamic-programming/6.8 数据结构优化DP/题解-赤壁之战-平衡树优化.md',
+			'/solution/06.dynamic-programming/6.9 数据结构优化DP/题解-清理班次-线段树优化.md',
+			'/solution/06.dynamic-programming/6.9 数据结构优化DP/题解-清理班次2-线段树优化.md',
+			'/solution/06.dynamic-programming/6.9 数据结构优化DP/题解-赤壁之战-平衡树优化.md',
         ],
     }, {
         title: '单调队列优化DP',
         children: [
-			'/solution/06.dynamic-programming/6.9 单调队列优化DP/题解-围栏-单调队列优化.md',
+			'/solution/06.dynamic-programming/6.10 单调队列优化DP/题解-围栏-单调队列优化.md',
         ],
     }, {
         title: '斜率优化',
         children: [
-			'/solution/06.dynamic-programming/6.10 斜率优化/题解-任务安排1-费用提前计算.md',
-			'/solution/06.dynamic-programming/6.10 斜率优化/题解-任务安排2-斜率优化.md',
-			'/solution/06.dynamic-programming/6.10 斜率优化/题解-任务安排3-斜率优化.md',
-			'/solution/06.dynamic-programming/6.10 斜率优化/题解-运输小猫-斜率优化.md',
+			'/solution/06.dynamic-programming/6.11 斜率优化/题解-任务安排1-费用提前计算.md',
+			'/solution/06.dynamic-programming/6.11 斜率优化/题解-任务安排2-斜率优化.md',
+			'/solution/06.dynamic-programming/6.11 斜率优化/题解-任务安排3-斜率优化.md',
+			'/solution/06.dynamic-programming/6.11 斜率优化/题解-运输小猫-斜率优化.md',
         ],
     }, {
         title: '四边形不等式',
         children: [
-			'/solution/06.dynamic-programming/6.11 四边形不等式/题解-再探石子合并-四边形不等式优化.md',
+			'/solution/06.dynamic-programming/6.12 四边形不等式/题解-再探石子合并-四边形不等式优化.md',
         ],
     }, {
         title: '计数DP',
         children: [
-			'/solution/06.dynamic-programming/6.12 计数DP/题解-装饰围栏-计数DP.md',
-			'/solution/06.dynamic-programming/6.12 计数DP/题解-低价购买-计数DP.md',
-			'/solution/06.dynamic-programming/6.12 计数DP/题解-数的划分-计数DP.md',
+			'/solution/06.dynamic-programming/6.13 计数DP/题解-装饰围栏-计数DP.md',
+			'/solution/06.dynamic-programming/6.13 计数DP/题解-低价购买-计数DP.md',
+			'/solution/06.dynamic-programming/6.13 计数DP/题解-数的划分-计数DP.md',
         ],
     }, {
         title: '数位统计DP',
         children: [
-			'/solution/06.dynamic-programming/6.13 数位统计DP/题解-启示录-数位DP.md',
-			'/solution/06.dynamic-programming/6.13 数位统计DP/题解-月之谜-数位DP.md',
+			'/solution/06.dynamic-programming/6.14 数位统计DP/题解-启示录-数位DP.md',
+			'/solution/06.dynamic-programming/6.14 数位统计DP/题解-月之谜-数位DP.md',
         ],
     }],
 }
